01039e9333ad0e288573662783247f0481c10abc
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-11 14:50:46 +0200

    Exercise Mail: Texual code set

---
+++ Mail/js/script.js
@@ -0,0 +1,19 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.*/
+
+//creo una raccolta di mail autorizzate all'accesso
+
+//chiedo all'utente la sua mail e salvo la risposta in una variabile
+
+//creo una variabile globale di valore booleano false, che diverrà true ad un evento
+
+//controllo la lista di mail, una alla volta in un ciclo.
+
+//SE la mail fornita dall'utente corrisponderà ad una nella lista la variabile globale diventa true
+
+//altrimenti resterà false
+
+//finito il ciclo di controllo, SE la variabile globale sarà divenuta true, sarà loggato un messaggio affermativo
+
+//altrimenti un messaggio negativo.
