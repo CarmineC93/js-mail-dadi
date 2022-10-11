27c21b0fd6e865957b3adb0e10c0a031a485e7ac
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-10-11 15:23:49 +0200

    Excercise Dice Game: Texual code set

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,11 @@
+/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+//Chiedo all'utente/giocatore un numero da 1 a 6, e salvo la risposta in un variabile
+
+//salvo in una variabile il numero da 1 a 6 del computer, tramite math.random 
+
+//Se il numero del giocatore è superiore a quello del computer, l'utente ha vinto
+    //se invece è minore, il computer ha vinto,
+      //altrimenti c'è pareggio
+
